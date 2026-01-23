ed0df77dd189ac0d68a83938fad84763befe6c6c
MalikShahry <malikshahry23@gmail.com>
2026-01-22 17:03:52 -0700

    Added SignalR & SignalR.Core package

--- src/FleetPulse.Api/obj/Debug/net10.0/FleetPulse.Api.AssemblyInfo.cs
+++ src/FleetPulse.Api/obj/Debug/net10.0/FleetPulse.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetPulse.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bfb9a1bbdf95368572270e30771ab74ad046c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1ae528b7f028370d56ea9bc8527ea813e5322e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetPulse.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetPulse.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
