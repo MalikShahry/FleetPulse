a729d3f0bf85c985773b078ec896db7e37bd2f4a
MalikShahry <malikshahry23@gmail.com>
2026-01-27 20:48:16 -0700

    Created a fix path for the delivery vehicle to move along

--- src/FleetPulse.Simulator/Program.cs
+++ src/FleetPulse.Simulator/Program.cs
@@ -6,16 +6,34 @@ var client = new HttpClient { BaseAddress = new Uri("http://localhost:5035/") };
 
 var rand = new Random();
 
+var path = new List<(double Lat, double Lon)>
+{
+    (51.0450, -114.0570),
+    (51.0465, -114.0600),
+    (51.0480, -114.0625),
+    (51.0495, -114.0600),
+    (51.0505, -114.0570),
+    (51.0485, -114.0540),
+    (51.0460, -114.0555),
+};
 
-double latitude = 51.045;
-double longitude = -114.057;
-
+int targetIndex = 1;
+double latitude = path[0].Lat;
+double longitude = path[0].Lon;
+double stepSize = 0.0002; //20m per update
 
 while (true)
 {
     // simulate movement
-    latitude += (rand.NextDouble() - 0.5) * 0.001;
-    longitude += (rand.NextDouble() - 0.5) * 0.001;
+
+    var target = path[targetIndex];
+
+    (latitude, longitude) = MoveTowards(
+        latitude, 
+        longitude, 
+        target.Lat, 
+        target.Lon, 
+        stepSize);
 
 
     var body = new
@@ -26,7 +44,31 @@ while (true)
 
     await client.PostAsJsonAsync($"/api/vehicles/{vehicleId}/location", body);
 
-    Console.WriteLine($"Sent location: {latitude}, {longitude}");
+    Console.WriteLine($"Sent location for Vehicle {vehicleId} at: {latitude}, {longitude}");
 
     await Task.Delay(2000);
+}
+
+static (double lat, double lon) MoveTowards(
+    double currentLat, 
+    double currentLon, 
+    double targetLat, 
+    double targetLon, 
+    double stepSize)
+{
+    double dLat = targetLat - currentLat;
+    double dLon = targetLon - currentLon;
+    double distance = Math.Sqrt(dLat * dLat + dLon * dLon);
+
+    if (distance <= stepSize || distance == 0)
+    {
+        return (targetLat, targetLon);
+    }
+    else
+    {
+        double ratio = stepSize / distance;
+        double newLat = currentLat + dLat * ratio;
+        double newLon = currentLon + dLon * ratio;
+        return (newLat, newLon);
+    }
 }
